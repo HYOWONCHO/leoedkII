fa163be8928700dc378cafc2df843ca05d1982c5
LeoC <rocketmancho@outlook.com>
2025-05-21 10:23:58 +0900

    File write and create adds

--- LeoTest/SBC_FileCtrl.h
+++ LeoTest/SBC_FileCtrl.h
@@ -15,11 +15,41 @@
  * \param[in] FileNames
  * \param[out] out
  * 
- * \return EFI_STATUS 
+ * \return EFI_STATUS
+ * \note
+ *  MUST call the SBC_FileSysFindHndl to obtain a FileProtocolHandle before using this function
  */
 EFI_STATUS SBC_ReadFile(EFI_HANDLE ImageHandle, CHAR16 *FileNames, LV_t *out);
 
 SBCStatus  SBC_GetFileSize(CHAR16 *FileName, UINTN  *FileSize);
 
 UINTN  SBC_FileSysFindHndl(EFI_HANDLE *handle);
+
+/*!
+ * Create the File 
+ * 
+ * \author leoc (5/21/25)
+ * 
+ * \param h      
+ * \param fname  
+ * 
+ * \return SBCStatus
+ * \note
+ *  MUST call the SBC_FileSysFindHndl to obtain a FileProtocolHandle before using this function
+ */
+SBCStatus  SBC_CreateFile(EFI_HANDLE h, CHAR16 *fname);
+
+/*!
+ * Create the directory
+ * 
+ * \author leoc (5/21/25)
+ * 
+ * \param h      
+ * \param fname  
+ * 
+ * \return SBCStatus
+ * \note
+ *  MUST call the SBC_FileSysFindHndl to obtain a FileProtocolHandle before using this function
+ */
+SBCStatus  SBC_CreateDirectory(EFI_HANDLE h, CHAR16 *fname);
 #endif
