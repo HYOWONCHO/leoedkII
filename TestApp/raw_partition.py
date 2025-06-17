34c225c555f405b4df045801031761d5064f60f0
LeoC <rocketmancho@outlook.com>
2025-06-17 14:46:03 +0900

    x
    Former-commit-id: 15bbd2f45eb3f486ed071bbf33059df9c0758548

--- TestApp/raw_partition.py
+++ TestApp/raw_partition.py
@@ -1,5 +1,6 @@
 import sys
 import hashlib
+import os
 
 import hw_info
 import file_ctrl
@@ -15,16 +16,7 @@ def compute_device_id(buffers):
         sha256.update(buffer)
     return sha256.hexdigest()
 
-
-
-
-if __name__ == "__main__":
-    baseboard_sn = hw_info.get_baseboard_sn()
-    memory_sn = hw_info.get_memory_serial()
-    ssd_sn = hw_info.get_ssd_serial()
-
-    hw_unique_info_print(baseboard_sn, memory_sn, ssd_sn)
-
+def read_fsblimg():
     fname = "./FSBL.efi"
     if file_ctrl.file_is_exists(fname) != True:
         print("File doee not exists")
@@ -35,9 +27,42 @@ if __name__ == "__main__":
     flen = file_ctrl.get_file_length(fname)
     rdimg = file_ctrl.read_file(fname)
 
-    devid = compute_device_id([baseboard_sn, memory_sn, ssd_sn, rdimg])
+    return rdimg
+
+def read_ssblimg():
+    fname = "./SSBL.efi"
+    if file_ctrl.file_is_exists(fname) != True:
+        print("File doee not exists")
+        sys.exit()
+    else:
+        print("File Exists")
+
+    flen = file_ctrl.get_file_length(fname)
+    rdimg = file_ctrl.read_file(fname)
+
+    return rdimg
+
+
+if __name__ == "__main__":
+
+    fname = "./test_raw.bin"
+
+    baseboard_sn = hw_info.get_baseboard_sn()
+    memory_sn = hw_info.get_memory_serial()
+    ssd_sn = hw_info.get_ssd_serial()
+
+    hw_unique_info_print(baseboard_sn, memory_sn, ssd_sn)
+
+
+    rdimg = read_fsblimg()
+    devidlist = [baseboard_sn.encode(), memory_sn.encode(), ssd_sn.encode(), rdimg.encode()]
+    devid = compute_device_id(devidlist)
     print(devid)
+
+    os.remove(fname);
     
+    file_ctrl.write_header_info(fname)
+
 
 
 
