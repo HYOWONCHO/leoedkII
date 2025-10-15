9046f5322ad0aa77254bb8b6015ecc15b6a87ba8
LeoC <rocketmancho@outlook.com>
2025-10-15 15:26:19 +0900

    x
    Former-commit-id: fd433603b56a27b3170bf48da19042ea006ff5c9

---
+++ SBCPkg/Application/FSBL/SBC_Timer.c
@@ -0,0 +1,32 @@
+#include <Uefi.h>
+#include <Library/TimerLib.h>
+#include <Library/PrintLib.h>
+#include <Library/DebugLib.h>
+
+
+UINTN _PerfDeltaTicks(UINTN T0, UINTN T1)
+{
+    UINT64 Start, End;
+    (void)GetPerformanceCounterProperties(&Start, &End);
+
+    if (Start < End) { // count-up
+        if (T1 >= T0) return T1 - T0;
+        // rollover
+        return (End - T0) + (T1 - Start) + 1;
+    } else {           // count-down
+        if (T1 <= T0) return T0 - T1;
+        // rollover
+        return (T0 - End) + (Start - T1) + 1;
+    }
+}
+
+UINTN SBC_PerfNowTicks(VOID)
+{
+    return GetPerformanceCounter();
+}
+
+UINTN SBC_PerfTicksTons(UINTN ticks)
+{
+    return GetTimeInNanoSecond(ticks);
+}
+
