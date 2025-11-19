f2cd226f0c87604dde351be238e0f4d0bb645cc3
LeoC <rocketmancho@outlook.com>
2025-11-20 08:24:57 +0900

    MpTool related draft version adds
    Former-commit-id: ca9ccde0804da6f96496608c536923a01d835e60

---
+++ SBCPkg/Application/MpTool/src/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <stdint.h>
+
+
+
+int main(int argc, char *argv[])
+{
+
+
+    printf("Hello world \n");
+
+    return 0;
+
+}
