1f49f6c2cf57df208fbe4d55383c1808518c8f43
LeoC <rocketmancho@outlook.com>
2025-11-20 12:37:43 +0900

    MP tool raw read and write
    Former-commit-id: 42adfe2aede6aaff253d3e3ba221cc459a185cb0

--- SBCPkg/Application/MpTool/src/main.c
+++ SBCPkg/Application/MpTool/src/main.c
@@ -3,15 +3,44 @@
 #include <string.h>
 #include <errno.h>
 #include <stdint.h>
+#include <getopt.h>
 
 
+#include "opt.h"
 
 int main(int argc, char *argv[])
 {
 
+    struct cmd_ctx ctx;
 
-    printf("Hello world \n");
+    if(parse_opts(argc, argv, &ctx))
+        return -1;
+
+    switch (ctx.type) {
+
+        case CMD_DUMPIMG:
+            printf("dumpimg: addr=0x%lx size=%lu\n",
+                    ctx.dump_addr, ctx.dump_size);
+            break;
+
+        case CMD_LOADIMG:
+            printf("loadimg: file=%s addr=0x%lx size=%lu\n",
+                    ctx.file, ctx.load_addr, ctx.load_size);
+            break;
+
+        case CMD_SETPRES:
+            printf("setpres: %d %c %d %c\n",
+                    ctx.n1, ctx.c1, ctx.n2, ctx.c2);
+            break;
+
+        case CMD_GETPRES:
+            printf("getpres\n");
+            break;
+
+        default:
+            print_usage(argv[0]);
+            break;
+    }
 
     return 0;
-
 }
